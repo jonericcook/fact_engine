aaa0ec42423da56cd292808b454f31dc510f375d
Jon-Eric Cook <joneric.cook@blockfi.com>
2022-07-20 08:25:39 -0700

    first commit

---
+++ lib/fact_engine.ex
@@ -0,0 +1,18 @@
+defmodule FactEngine do
+  @moduledoc """
+  Documentation for `FactEngine`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FactEngine.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
