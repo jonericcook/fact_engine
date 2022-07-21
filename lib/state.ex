e68811bd2541ee0015370b14fff58fd84c240823
Jon-Eric Cook <joneric.cook@blockfi.com>
2022-07-21 13:28:20 -0700

    save work

--- lib/state.ex
+++ lib/state.ex
@@ -1,27 +1,59 @@
 defmodule State do
   use Agent
 
-  def init() do
+  def start() do
     case Agent.start_link(fn -> %{} end,
            name: {:global, :state}
          ) do
-      {:ok, pid} -> {:ok, pid}
-      {:error, {:already_started, pid}} -> {:ok, pid}
+      {:ok, _pid} -> :ok
+      {:error, {:already_started, _pid}} -> :ok
+    end
+  end
+
+  def reset() do
+    case stop() do
+      {:error, :no_state_to_stop} ->
+        start()
+
+      _ ->
+        start()
+    end
+  end
+
+  def stop() do
+    case :global.whereis_name(:state) do
+      :undefined ->
+        {:error, :no_state_to_stop}
+
+      pid when is_pid(pid) ->
+        Agent.stop(pid)
+    end
+  end
+
+  defp pid() do
+    case :global.whereis_name(:state) do
+      pid when is_pid(pid) ->
+        pid
     end
   end
 
   def get() do
-    {:ok, state} = pid()
-
-    Agent.get(state, fn state ->
+    Agent.get(pid(), fn state ->
       state
     end)
   end
 
-  def pid() do
-    case :global.whereis_name(:state) do
-      pid when is_pid(pid) ->
-        {:ok, pid}
-    end
+  def get(statement) do
+    Agent.get(pid(), fn state ->
+      Map.get(state, statement, [])
+    end)
+  end
+
+  def update(statement, arguments) do
+    Agent.update(pid(), fn state ->
+      Map.update(state, statement, MapSet.new([arguments]), fn existing_value ->
+        MapSet.put(existing_value, arguments)
+      end)
+    end)
   end
 end
